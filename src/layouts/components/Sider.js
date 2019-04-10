47e8a0626864c39dc37df70e1c104d560320f61f
fff.com <fff.com>
2019-04-10 16:53:21 +0800

    delete menu to avoid uncollapsed menu

--- src/layouts/components/Sider.js
+++ src/layouts/components/Sider.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
-import Menulist from './Menu';
+import { Layout, Menu, Icon } from 'antd';
+import Link from 'umi/link';
 import Logo from './Logo';
 import styles from './Sider.less';
 import { connect } from 'dva';
@@ -18,7 +18,34 @@ class Sider extends Component {
   }
 
   render() {
-    const { collapsed } = this.props
+    const { collapsed, submenus } = this.props
+    const menu = submenus.map((items) => {
+      if(items.hasOwnProperty('children')) {
+        const menuItem = Object.keys(items.children).map((_, index) => {
+          return (
+            <Menu.Item key={items.key + index}><Link to={_}>{items.children[_]}</Link></Menu.Item>
+          )
+        })
+        return (
+          <Menu.SubMenu
+            key={items.link}
+            title={<span><Icon type={items.icon} /><span>{items.name}</span></span>}
+          >
+            {menuItem}
+          </Menu.SubMenu>
+        )
+      }
+      else {
+        return (
+          <Menu.Item key={items.key}>
+            <Link to={items.link}>
+              <Icon type={items.icon} />
+              <span>{items.name}</span>
+            </Link>
+          </Menu.Item>
+        )
+      }
+    })
     return (
       <Layout.Sider
         className={styles.sider}
@@ -32,12 +59,15 @@ class Sider extends Component {
         theme="light"
       >
         <Logo collapsed={collapsed} />
-        <Menulist />
+        <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+          {menu}
+        </Menu>
       </Layout.Sider>
     );
   }
 }
 
-export default connect(({ header }) => ({
+export default connect(({ header, urlRouter }) => ({
   collapsed: header.collapsed,
+  submenus: urlRouter.submenus,
 }))(Sider)
